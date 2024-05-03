eac8710a5bf2e976f5e294ea3a61a0e1a810182d
tcampbell01 <teresa.j.campbell@gmail.com>
2024-05-03 18:50:42 -0400

    variables

---
+++ Day 1/variables.py
@@ -0,0 +1,16 @@
+#This program takes two inputs.  The first input is stored in a variable called a.  The second input is stored in a variable called b. Write a program that switches the values stored in the variables a and b.  
+
+# There are two variables, a and b from input
+a = input()
+b = input()
+# 🚨 Don't change the code above ☝️
+####################################
+# Write your code below this line 👇
+c = a
+a = b
+b = c
+
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)

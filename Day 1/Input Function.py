898ad3d3aee0ed8b28a50a24bfa51b611f8ee06d
tcampbell01 <teresa.j.campbell@gmail.com>
2024-05-03 18:48:47 -0400

    input function

---
+++ Day 1/Input Function.py
@@ -0,0 +1,5 @@
+#Write a program that calculates and outputs the number of characters in any name.  The automated tests will try out lots of different names as the input.  Your code should work for any name.  Your code should only output the number, no other text is needed.  
+
+length = len(input())
+
+print(length)
